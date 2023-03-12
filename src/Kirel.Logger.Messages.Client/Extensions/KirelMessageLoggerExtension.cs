db2a826e22978ebc761a3593df32e3deb68c4571
lordspinach <ksgamayunov@gmail.com>
2023-03-12 13:19:31 +0300

    messages: client: message logger now uses logger provider

---
+++ src/Kirel.Logger.Messages.Client/Extensions/KirelMessageLoggerExtension.cs
@@ -0,0 +1,34 @@
+﻿using Kirel.Logger.Messages.Client.Logger;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+
+namespace Kirel.Logger.Messages.Client.Extensions;
+
+public static class KirelMessageLoggerExtension
+{
+    public static ILoggingBuilder AddKirelMessageLogger(
+        this ILoggingBuilder builder)
+    {
+        builder.AddConfiguration();
+
+        builder.Services.TryAddEnumerable(
+            ServiceDescriptor.Singleton<ILoggerProvider, KirelMessageLoggerProvider>());
+
+        LoggerProviderOptions.RegisterProviderOptions
+            <KirelMessageLoggerConfiguration, KirelMessageLoggerProvider>(builder.Services);
+
+        return builder;
+    }
+
+    public static ILoggingBuilder AddKirelMessageLogger(
+        this ILoggingBuilder builder,
+        Action<KirelMessageLoggerConfiguration> configure)
+    {
+        builder.AddKirelMessageLogger();
+        builder.Services.Configure(configure);
+
+        return builder;
+    }
+}
